d15d3bef23c49d45998d80384dbef8d5576e6b45
VoytovichKlimentiy <Tyuiu.VoytovichKA@outlook.com>
2026-09-11 23:53:34 +0500

    Task4

---
+++ Tyuiu.VoytovichKA.Sprint1.Task4.V4.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.VoytovichKA.Sprint1.Task4.V4.Lib;
+
+namespace Tyuiu.VoytovichKA.Sprint1.Task4.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void IsAccurate()
+        {
+            DataService ds = new DataService();
+            double x = -5, y = -4;
+            double res = ds.Calculate(x, y);
+            Assert.AreEqual(7, res);
+        }
+    }
+}
---
+++ Tyuiu.VoytovichKA.Sprint1.Task4.V4/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.VoytovichKA.Sprint1.Task4.V4.Lib;
+
+namespace Tyuiu.VoytovichKA.Sprint1.Task4.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Войтович К.А. | НТм-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Операторы составного присаивания                                  *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #9                                                              *");
+            Console.WriteLine("* Выполнил: Войтович Климентий Антонович | НТм-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле и печатает его на экране.                *");
+            Console.WriteLine("* Формула:                                                                *");
+            Console.WriteLine("* 1 + x * y                                                               *");
+            Console.WriteLine("* ---------                                                               *");
+            Console.WriteLine("* | x + 2 |                                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            double x;
+            double y;
+            Console.WriteLine("Введите x:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите y:");
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate(x,y));
+            Console.ReadLine();
+        }
+    }
+}
