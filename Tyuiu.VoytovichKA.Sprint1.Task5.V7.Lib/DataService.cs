939568fc90b7f3a4ed2dc5fb17d4dd49bbd05cd5
VoytovichKlimentiy <Tyuiu.VoytovichKA@outlook.com>
2026-09-12 22:12:57 +0500

    Task5

---
+++ Tyuiu.VoytovichKA.Sprint1.Task5.V7.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.VoytovichKA.Sprint1.Task5.V7.Lib
+{
+    public class DataService : ISprint1Task5V7
+    {
+        public int AngleToHoursMinutes(double f)
+        {
+            return Convert.ToInt16(f / 30);
+        }
+    }
+}
---
+++ Tyuiu.VoytovichKA.Sprint1.Task5.V7.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.VoytovichKA.Sprint1.Task5.V7.Lib;
+
+namespace Tyuiu.VoytovichKA.Sprint1.Task5.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void IsValid()
+        {
+            DataService ds = new DataService();
+            int res = ds.AngleToHoursMinutes(67);
+            Assert.AreEqual(2, res);
+            
+        }
+    }
+}
